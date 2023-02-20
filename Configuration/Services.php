e5590b0396d2d2dd7e535b4108677998caf4e927
Laurent FOULLOY <laurent.foulloy@gmail.com>
2023-02-20 18:16:27 +0100

    File corrected

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -2,18 +2,16 @@
 
 return static function (\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $configurator, \Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder): void {
 
-    if (file_exists('typo3conf/ext/sav_library_mvc/Configuration/')) {
-        $configurationDirectory =  'typo3conf/ext/sav_library_mvc/Configuration/';
-    } elseif (file_exists('../typo3conf/ext/sav_library_mvc/Configuration/')) {
-        $configurationDirectory =  '../typo3conf/ext/sav_library_mvc/Configuration/';
+    $configurationDirectory =  \TYPO3\CMS\Core\Core\Environment::getExtensionsPath() . '/sav_library_mvc/Configuration/';
+    if (! file_exists($configurationDirectory)) {
+        throw new \Exception('Configuration directory not found in sav_library_mvc.');
     } else {
-        die('configuration directory not found in sav_library_mvc');
-    }
-    $typo3Version = new \TYPO3\CMS\Core\Information\Typo3Version;
-    $yamlFileLoader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader ($containerBuilder, new \Symfony\Component\Config\FileLocator($configurationDirectory));
-    if (version_compare($typo3Version->getVersion(), '11.0', '>')) {
-        $yamlFileLoader->load('ServicesV11.yaml');
-    } else {
-        $yamlFileLoader->load('ServicesV10.yaml');
+        $typo3Version = new \TYPO3\CMS\Core\Information\Typo3Version;
+        $yamlFileLoader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader ($containerBuilder, new \Symfony\Component\Config\FileLocator($configurationDirectory));
+        if (version_compare($typo3Version->getVersion(), '11.0', '>')) {
+            $yamlFileLoader->load('ServicesV11.yaml');
+        } else {
+            $yamlFileLoader->load('ServicesV10.yaml');
+        }
     }
 };
